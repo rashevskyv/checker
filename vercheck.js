db6ce7efe8c389b484363a0f49ca08779893ae7c
rashevskyv <rashevsky.v@gmail.com>
2018-11-22 12:55:40 +0200

    Add css

---
+++ vercheck.js
@@ -0,0 +1,86 @@
+    function myFunction() {
+        var x = document.getElementById("nxsn");
+        if (x.validity.valid && x.value.length > 7) {
+            var series = x.value.substring(2, 4).toUpperCase();
+            var sn;
+            switch (series) {
+                case 'W1':
+                    sn = x.value.substring(4, 8);
+                    if (sn.localeCompare('01') >= 0) {
+                        document.getElementById("result").innerHTML = '<div class="failed">Уязвимость устранена</div></div>';
+                    } else if (sn.localeCompare('0079') >= 0) {
+                        document.getElementById("result").innerHTML = '<div class="maybe">Уязвимость возможно устранена</div>';
+                    } else if (sn.localeCompare('0078') <= 0) {
+                        document.getElementById("result").innerHTML = '<div class="passed">Уязвимость присутствует</div>';
+                    } else {
+                        document.getElementById("result").innerHTML = '<div class="unknown">Нет информации</div>';
+                    }
+                    break;
+                case 'W4':
+                    sn = x.value.substring(4, 8);
+                    if (sn.localeCompare('0013') >= 0) {
+                        document.getElementById("result").innerHTML = '<div class="failed">Уязвимость устранена</div>';
+                    } else if (sn.localeCompare('0012') >= 0) {
+                        document.getElementById("result").innerHTML = '<div class="maybe">Уязвимость возможно устранена</div>';
+                    } else if (sn.localeCompare('0011') <= 0) {
+                        document.getElementById("result").innerHTML = '<div class="passed">Уязвимость присутствует</div>';
+                    } else {
+                        document.getElementById("result").innerHTML = '<div class="unknown">Нет информации</div>';
+                    }
+                    break;
+                case 'W7':
+                    if (x.value.length > 8) {
+                        sn = x.value.substring(4, 9);
+                        if (sn.localeCompare('0019') >= 0) {
+                            document.getElementById("result").innerHTML = '<div class="failed">Уязвимость устранена</div>';
+                        } else if (sn.localeCompare('00179') >= 0) {
+                            document.getElementById("result").innerHTML = '<div class="maybe">Уязвимость возможно устранена</div>';
+                        } else if (sn.localeCompare('00178') <= 0) {
+                            document.getElementById("result").innerHTML = '<div class="passed">Уязвимость присутствует</div>';
+                        } else {
+                            document.getElementById("result").innerHTML = '<div class="unknown">Нет информации</div>';
+                        }
+                    } else {
+                        document.getElementById("result").innerHTML = "";
+                    }
+                    break;
+                case 'J1':
+					sn = x.value.substring(4, 8);
+                    if (sn.localeCompare('0031') >= 0) {
+                        document.getElementById("result").innerHTML = '<div class="maybe">Уязвимость возможно устранена</div>';
+                    } else if (sn.localeCompare('0030') <= 0) {
+                        document.getElementById("result").innerHTML = '<div class="passed">Уязвимость присутствует</div>';
+                    } else {
+                        document.getElementById("result").innerHTML = '<div class="unknown">Нет информации</div>';
+                    }
+                    break;
+                case 'J4':
+                    sn = x.value.substring(4, 8);
+                    if (sn.localeCompare('006') >= 0) {
+                        document.getElementById("result").innerHTML = '<div class="failed">Уязвимость устранена</div>';
+                    } else if (sn.localeCompare('0051') >= 0) {
+                        document.getElementById("result").innerHTML = '<div class="maybe">Уязвимость возможно устранена</div>';
+                    } else if (sn.localeCompare('0050') <= 0) {
+                        document.getElementById("result").innerHTML = '<div class="passed">Уязвимость присутствует</div>';
+                    } else {
+                        document.getElementById("result").innerHTML = '<div class="unknown">Нет информации</div>';
+                    }
+                    break;
+                case 'J7':
+                    sn = x.value.substring(4, 8);
+                    if (sn.localeCompare('005') >= 0) {
+                        document.getElementById("result").innerHTML = '<div class="failed">Уязвимость устранена</div>';
+                    } else if (sn.localeCompare('0043') >= 0) {
+                        document.getElementById("result").innerHTML = '<div class="maybe">Уязвимость возможно устранена</div>';
+                    } else if (sn.localeCompare('0042') <= 0) {
+                        document.getElementById("result").innerHTML = '<div class="passed">Уязвимость присутствует</div>';
+                    } else {
+                        document.getElementById("result").innerHTML = '<div class="unknown">Нет информации</div>';
+                    }
+                    break;
+            }
+        }
+        else {
+            document.getElementById("result").innerHTML = "";
+        }
+    }
