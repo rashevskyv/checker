2e0a6fe2789824ecdf405a7811d6caff9d7e0b93
rashevskyv <rashevsky.v@gmail.com>
2019-10-17 11:00:14 +0300

    17.10

--- vercheck.js
+++ vercheck.js
@@ -60,7 +60,7 @@
                     sn = x.value.substring(4, 10);
                     if (sn.localeCompare('008278') == 0) {
                         document.getElementById("result").innerHTML = '<div class="passed">Вероятнее всего, уязвимость присутствует.<br>Вы можете прошить эту приставку</div>';
-                    } else if (sn.localeCompare('006') >= 0) {
+                    } else if (sn.localeCompare('0083') >= 0) {
                         document.getElementById("result").innerHTML = '<div class="failed">Уязвимость устранена<br>Вы не сможете прошить приставку!<br><br>Однако, если версия системного ПО на консоли 4.1.0, то её можно прошить с помощью <a href="http://switch.customfw.xyz/caffeine">Caffeine</div>';
                     } else if (sn.localeCompare('0046') >= 0) {
                         document.getElementById("result").innerHTML = '<div class="maybe">Уязвимость возможно устранена<br>Если вы собираетесь купить эту консоль, подумайте ещё раз, если же уже купили, просто попробуйте</div><p>Единственный верный способ, который поможет понять прошивается ваша приставка или нет - <a href="http://switch.customfw.xyz/fusee-gelee">попробовать</a>. Если приставка прошиваемая, она запустит переданный ей пейлоад. Если нет, экран останется чёрным, хотя в окне tegrarcmGUI и будет показано, что пейлоад проброшен успешно: <br><br>Значение <strong>0x0000</strong> в логе программы означает, что вашу приставку нельзя прошить с помощью <a href="http://switch.customfw.xyz/fusee-gelee">Fusée Gelée</a><br><br><img src="http://check.customfw.xyz/tegra.png"></p>';
