e8bbb7093cbac636369292c83be28cec166e93ce
rashevskyv <rashevsky.v@gmail.com>
2020-08-03 11:01:51 +0300

    Rebuilt script

--- src/static/js/main.js
+++ src/static/js/main.js
@@ -6,9 +6,12 @@
 		this.$wrapper = this.$container.closest('.table__wrapper');
 		this.resultClass = this.$container.data('result');
 		this.result = this.$wrapper.next('.result');
+		
 		this.result1Content = '<p class="checker_p"><strong>Введите первые 9 символов серийного номера консоли.</strong></p><p class="checker_p">Серийный номер консоли расположен на нижней грани самого планшета. Если приставка б\у, то убедитесь, что серийный номер на корпусе совпадает с тем, что указан в <strong>Системных настройках</strong> -> <strong>Система</strong> -> <strong>Серийные номера</strong>. Если после ввода серийника ничего не происходит, отключите блокировщик рекламы<div class="checker"><input class="inputtext" type="text" id="nxsn" name="country_code" pattern="[xX][aAkK][wWjJkK](1|4|7|9)[0-9]+" placeholder="XAW100000" onkeyup="checkSerial()"></div>';
 
 		this.result2Content = '<div class="new_revision checker_p">Nintendo Switch Lite прошивается только с помощью чипа SX Lite</div><p class="checker_p"><strong>Подробнее про чипы <a href="https://sx.customfw.xyz/">на сайте нашего магазина</a>. Там же можно сделать заказ.</strong></p>';
+
+		this.resultEmpty = '';
 };
 
 $.extend(true, Component.prototype, {
@@ -19,8 +22,10 @@ $.extend(true, Component.prototype, {
 	addContent() {
 		if (this.resultClass === 'result1') {
 			this.result.html(this.result1Content);
+			$(".check-result").html("");
 		} else if (this.resultClass === 'result2') {
 			this.result.html(this.result2Content);
+			$(".check-result").html("");
 		}
 	},
 
@@ -28,15 +33,14 @@ $.extend(true, Component.prototype, {
 		this.addContent() 
 		
 		this.result.slideDown(500);
-		$('.result').removeClass('hidden')
+			$('html, body').animate({
+				scrollTop: $(".result").offset().top
+			}, 600);		$('.result').removeClass('hidden')
 	},
 
 	_bindInteractions() {
 	this.$container.on('click', this.showResult.bind(this));
 	$(".descriptionToggler").click(function() {
-			$('html, body').animate({
-				scrollTop: $("#scroll_here").offset().top
-			}, 600);
 			$( ".inputtext" ).focus();
   		});
 	},
