2586506897fcf8eb1fc816f4b636f19e508d0112
rashevskyv <rashevsky.v@gmail.com>
2021-01-15 10:38:02 +0200

    Added more information about hack types, change links for new guide proposes

--- src/static/js/main.js
+++ src/static/js/main.js
@@ -9,7 +9,7 @@
 		
 		this.result1Content = '<p class="checker_p"><strong>Введите первые 9 символов серийного номера консоли.</strong></p><p class="checker_p">Серийный номер консоли расположен на нижней грани самого планшета. Если приставка б\у, то убедитесь, что серийный номер на корпусе совпадает с тем, что указан в <strong>Системных настройках</strong> -> <strong>Система</strong> -> <strong>Серийные номера</strong>. Если после ввода серийника ничего не происходит, отключите блокировщик рекламы<div class="checker"><input class="inputtext" type="text" id="nxsn" name="country_code" pattern="[xX][aAkK][wWjJkK](1|4|7|9)[0-9]+" placeholder="XAW100000" onkeyup="checkSerial()"></div>';
 
-		this.result2Content = '<div class="new_revision checker_p">Nintendo Switch Lite прошивается только с помощью чипа SX Lite</div><p class="checker_p"><strong>Подробнее про чипы <a href="https://sx.customfw.xyz/">на сайте нашего магазина</a>. Там же можно сделать заказ.</strong></p>';
+		this.result2Content = '<div class="new_revision checker_p">Nintendo Switch Lite прошивается только с помощью чипа SX Lite</div><p class="checker_p"><strong>Подробнее про чипы <a href="https://switch.customfw.xyz/modchip">в руководстве</a>. Там же можно сделать заказ.</strong></p>';
 
 		this.resultEmpty = '';
 };
